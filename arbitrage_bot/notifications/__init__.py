855fb412a545b2166d701cd1398dbee413f183dd
RohanKishibeCN <xenosaga1987@gmail.com>
2026-03-18 16:47:42 +0800

    Update __init__.py

--- arbitrage_bot/notifications/__init__.py
+++ arbitrage_bot/notifications/__init__.py
@@ -1,7 +1,3 @@
-"""通知模块"""
-
-from .notion import NotionNotifier
-from .qq import QQNotifier
 from .manager import NotificationManager
 
-__all__ = ['NotionNotifier', 'QQNotifier', 'NotificationManager']
+__all__ = ['NotificationManager']
